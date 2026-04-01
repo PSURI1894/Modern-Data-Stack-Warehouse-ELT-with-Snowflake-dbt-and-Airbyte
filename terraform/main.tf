f81320f1e5fc8df4e4a45986ab1e41257964e361
Sarah <parthsuri009@gmail.com>
2026-04-02 05:00:00 +0530

    tf: provision LOADING_WH and TRANSFORMING_WH compute engines

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,3 +8,22 @@ resource "snowflake_database" "analytics" {
   name    = "ANALYTICS"
   comment = "Analytics engineering database for staging, intermediate, and marts"
 }
+
+# Warehouses
+resource "snowflake_warehouse" "loading_wh" {
+  name           = "LOADING_WH"
+  warehouse_size = "X-SMALL"
+  auto_suspend   = 60
+  auto_resume    = true
+  comment        = "Warehouse for Airbyte raw syncs"
+}
+
+resource "snowflake_warehouse" "transforming_wh" {
+  name           = "TRANSFORMING_WH"
+  warehouse_size = "SMALL"
+  auto_suspend   = 60
+  auto_resume    = true
+  min_cluster_count = 1
+  max_cluster_count = 3
+  comment        = "Multi-cluster warehouse for dbt models execution"
+}
