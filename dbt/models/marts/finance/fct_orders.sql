e9e134c5d53a316f153c1e353cbf3d2f0ec073ca
Emily <parthsuri009@gmail.com>
2026-04-15 01:00:00 +0530

    feat(dbt): create fct_orders charges payments fact model

---
+++ dbt/models/marts/finance/fct_orders.sql
@@ -0,0 +1,20 @@
+with charges as (
+    select * from {{ ref('stg_stripe__charges') }}
+),
+customers as (
+    select * from {{ ref('int_customers__joined') }}
+),
+joined as (
+    select
+        charges.charge_id,
+        customers.customer_sk,
+        charges.stripe_customer_id,
+        charges.amount_cents,
+        charges.amount_dollars,
+        charges.currency,
+        charges.payment_status,
+        charges.charge_created_at as ordered_at
+    from charges
+    left join customers on charges.stripe_customer_id = customers.stripe_customer_id
+)
+select * from joined
